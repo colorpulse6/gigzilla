dc66c671ed898fc3f4c9d3ec835afe961e0b8a27
Nic Barnes <colorpulse@gmail.com>
2020-06-06 00:14:23 +0200

    basic routing finished except profile

--- routes/musician.routes.js
+++ routes/musician.routes.js
@@ -6,12 +6,12 @@ const VenueModel = require("../models/venue.model");
 const CityModel = require("../models/city.model");
 
 
-
+//Home Route
 router.get('/home/musician', (req, res) => {
     res.render('users/musician/musician-home.hbs', {layout: 'musicianLayout.hbs', musicianData: req.session.loggedInUser});
 })
-//Home Route Post
 
+//Home Route Post
 router.post('/home/musician', (req, res) => {
     let user = req.session.loggedInUser
     const {name} = req.body
@@ -23,14 +23,11 @@ router.post('/home/musician', (req, res) => {
                 .then((result) => {
                     res.redirect(`/home/musician/${tourData._id}`)
                 })
-                
         })
             
         .catch((err) => {
             console.log('This error is: ', err)
         })
-   
-
 })
 
 //Tour Route
@@ -46,14 +43,14 @@ router.get('/home/musician/:tours', (req, res) => {
     })
   
 })
-
+//Get City from Tour
 router.post('/home/musician/:tour', (req, res) => {
     const { city } = req.body
     const { tourId } = req.params
    
     VenueModel.find({cityName: city})
     .then((venues)=> {
-
+        //Create new City
         CityModel.create({
             name: city,
             venues: venues
@@ -75,15 +72,22 @@ router.post('/home/musician/:tour', (req, res) => {
 
 })
 
-//City Route
+//City Route (EDIT: Might not need all code)
 router.get('/home/musician/:tour/:cityId', (req, res) => {
     const { cityId } = req.params
+    
+    //GET CITY
     CityModel.findById(cityId)
-    .then(() => {
-        res.render('users/musician/musician-cities.hbs', {layout: 'musicianLayout.hbs', musicianData: req.session.loggedInUser});
+    .then((cities) => {
+        //GET VENUE
+        VenueModel.find({ "cityName": `${cities.name}`})
+        .then((venues) => {
+            res.render('users/musician/musician-cities.hbs', {layout: 'musicianLayout.hbs', musicianData: req.session.loggedInUser, cities, venues});
+            console.log(venues)
+        }) 
     })
-    .catch(() => {
-        res.send('Something is wrong')
+    .catch((err) => {
+        res.send('Something is wrong', err)
     })
     
 })
@@ -91,12 +95,13 @@ router.get('/home/musician/:tour/:cityId', (req, res) => {
 
 
 //Venue Route
-router.get('/home/musician/venues', (req, res) => {
-    res.render('users/musician/musician-venues.hbs', {layout: 'musicianLayout.hbs', musicianData: req.session.loggedInUser});
+router.get('/home/musician/:tour/:cityId/:venueId', (req, res) => {
+    const { venueId } = req.params;
+    VenueModel.findById(venueId)
+    .then((venueInfo) => {
+        res.render('users/musician/musician-venues.hbs', {layout: 'musicianLayout.hbs', musicianData: req.session.loggedInUser, venueInfo});
+    }) 
 })
-
-
-
 
 
 
