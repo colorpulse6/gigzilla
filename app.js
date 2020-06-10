97b43a97d02a5d80fab778c6bb5356a77b1e44ba
Michelle Lock <michelleloc@gmail.com>
2020-06-10 20:37:52 +0800

    second deploy

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(session({
     })
 }));
 
-// Express View engine setup
+// Express View engine setup test
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
