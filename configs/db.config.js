81790fc9626f5fc1f9eeb4e1af844f357bbc3bae
Michelle Lock <michelleloc@gmail.com>
2020-06-11 00:46:40 +0800

    Finished styles for landing and auth routes (main-layout stuff)

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/gigzilla';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://michelleytlock:coconuts@cluster0-aszcx.mongodb.net/gigzilla?retryWrites=true&w=majority';
 
 mongoose
   .connect(MONGODB_URI, {
